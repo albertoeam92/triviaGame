8db6a32aecb47593ea16e3f2186c303eb0ee873e
Alberto Alvarez <albertoealvarezm1992@gmail.com>
2016-10-27 22:30:41 -0400

    base files and assets added

---
+++ assets/javascript/app.js
@@ -0,0 +1,44 @@
+/* Javascript & jQuery starts here */
+
+var questions = {
+	question:  "Who is Princess Leia's father ?";
+	option: ["Senator Bail organa","Anakin skywalker","Luke Skywalker","Jabba the hut"];
+	answer: 1;
+	
+
+}, {
+	question: "What is the Jedis home planet ?";
+	option: ["Coruscant","tython","korriban","Dagobah"];
+	answer: 1;
+
+
+}, {
+	question: "What is Master Yoda age when he first met Luke Skywalker ?";
+	option: ["874 years old","890 years old","894 years old","814 years old"];
+	answer: 0;
+	
+
+}, {
+    question: "What is the name of the alien who got killed by Han solo in the cantina scene ?";
+    option: ["Jabba the Hut","Jar Jar Binks","Greedo","Ackbar"];
+    answer: 2;
+    
+
+}, {
+	question: "What's the name of the bounty hounter who gave his DNA in order to create the clone troopers ?";
+	option: ["Zam Wesell","IG-88","Embo","Jango Fett"];
+	answer: 3;
+	
+};
+
+
+$(document).ready(function(){
+
+
+	$("#start-button").click(function(){
+
+
+		
+	})
+
+});
